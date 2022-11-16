ce972b944a7c51291761f8cfad67dff8972ae6c5
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-16 12:31:19 +0100

    Add ERR_ALREADYREGISTRED to USER message

--- src/cmds/cmd_user.cpp
+++ src/cmds/cmd_user.cpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:01:23 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/16 12:06:07 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/16 12:30:55 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void cmd_user(CommandContext& context)
 {
-	(void)context;
-	// TODO
+	Client& client = context.client;
+
+	if (client.is_logged || client.info)
+		client.reply(ERR_ALREADYREGISTRED, ":Unauthorized command (already registered)");
+	else
+	{
+		// TODO
+	}
 }
