32044013a10ca82b515e678926670cb2fb68f8ba
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-16 14:34:19 +0100

    Add some functions in Channel

--- incl/Channel.hpp
+++ incl/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: estoffel <estoffel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 16:34:18 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/15 20:07:28 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/16 14:00:07 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,20 @@ class Channel {
 		~Channel();
 
 		typedef std::vector<Client*> ClientList;
-		
+
 		void 			broadcast(std::string const &message);
 
 		Server			*server;
 		std::string		topic;
 		std::string		password;
-	
+
 	private:
 		ClientList		_clients;
+
+	public:
+		void addClient(Client& client);
+		void removeClient(Client& client);
+		bool hasClient(Client& client);
 };
 
 #endif // CHANNEL_HPP
---
+++ src/Channel.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 14:00:38 by bbrassar          #+#    #+#             */
+/*   Updated: 2022/11/16 14:01:33 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Channel.hpp"
+
+void Channel::addClient(Client& client)
+{
+	// TODO
+	(void)client;
+}
+
+void Channel::removeClient(Client& client)
+{
+	// TODO
+	(void)client;
+}
+
+bool Channel::hasClient(Client& client)
+{
+	// TODO
+	(void)client;
+	return false;
+}
