75a87faba1e3deecc6a31d046047de3b173e58fb
enora stoffel <estoffel@Inception.42.fr>
2022-11-16 16:29:03 +0100

    colors

--- incl/Server.hpp
+++ incl/Server.hpp
@@ -6,26 +6,12 @@
 /*   By: estoffel <estoffel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 17:16:34 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/15 22:34:35 by estoffel         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:12:56 by estoffel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
 # define SERVER_HPP
-
-# define GREY "\033[0;30m"
-# define RORED "\033[0;31m"
-# define RED "\033[0;91m"
-# define GREEN "\033[0;32m"
-# define YELW "\033[0;33m"
-# define BLUE "\033[0;34m"
-# define PINK "\033[0;35m"
-# define TURQ "\033[0;36m"
-# define WHITE "\033[0;97m"
-# define BWHITE "\033[1;97m"
-# define LWHITE "\033[4;97m"
-# define IWHITE "\033[3;97m"
-# define END "\e[0m"
 
 # include "Client.hpp"
 # include "Channel.hpp"
--- incl/colours.h
+++ incl/colours.h
@@ -3,19 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   colours.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrandria <lrandria@student.42.fr>          +#+  +:+       +#+        */
+/*   By: estoffel <estoffel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 03:50:25 by lrandria          #+#    #+#             */
-/*   Updated: 2022/10/01 03:59:29 by lrandria         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:12:38 by estoffel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLOURS_H
 # define COLOURS_H
-
-                            //original file from paulahemsi
-                            
-                                    /*** TEXT  ***/
 
 /* normal */
 # define RED			"\e[0;31m"
@@ -43,24 +39,24 @@
 # define V_ORANGE		"\e[0;38;5;202m"
 
 /* bold */
-# define BOLD_RED		"\e[1;31m"
-# define BOLD_GREEN		"\e[1;32m"
-# define BOLD_BLUE		"\e[1;34m"
-# define BOLD_CYAN		"\e[1;36m"
-# define BOLD_MAGENTA	"\e[1;35m"
-# define BOLD_YELLOW	"\e[1;33m"
-# define BOLD_BLACK		"\e[1;30m"
-# define BOLD_WHITE		"\e[1;37m"
+# define B_RED			"\e[1;31m"
+# define B_GREEN		"\e[1;32m"
+# define B_BLUE			"\e[1;34m"
+# define B_CYAN			"\e[1;36m"
+# define B_MAGENTA		"\e[1;35m"
+# define B_YELLOW		"\e[1;33m"
+# define B_BLACK		"\e[1;30m"
+# define B_WHITE		"\e[1;37m"
 
 /* underline*/
-# define UNDER_RED		"\e[4;31m"
-# define UNDER_GREEN	"\e[4;32m"
-# define UNDER_BLUE		"\e[4;34m"
-# define UNDER_CYAN		"\e[4;36m"
-# define UNDER_MAGENTA	"\e[4;35m"
-# define UNDER_YELLOW	"\e[4;33m"
-# define UNDER_BLACK	"\e[4;30m"
-# define UNDER_WHITE	"\e[4;37m"
+# define U_RED			"\e[4;31m"
+# define U_GREEN		"\e[4;32m"
+# define U_BLUE			"\e[4;34m"
+# define U_CYAN			"\e[4;36m"
+# define U_MAGENTA		"\e[4;35m"
+# define U_YELLOW		"\e[4;33m"
+# define U_BLACK		"\e[4;30m"
+# define U_WHITE		"\e[4;37m"
 
 /* high intensity */
 # define H_RED			"\e[0;91m"
@@ -73,38 +69,38 @@
 # define H_WHITE		"\e[0;97m"
 
 /* bold high intensity */
-# define BOLD_H_BLACK	"\e[1;90m"
-# define BOLD_H_RED		"\e[1;91m"
-# define BOLD_H_GREEN	"\e[1;92m"
-# define BOLD_H_YELLOW	"\e[1;93m"
-# define BOLD_H_BLUE	"\e[1;94m"
-# define BOLD_H_MAGENTA	"\e[1;95m"
-# define BOLD_H_CYAN	"\e[1;96m"
-# define BOLD_H_WHITE	"\e[1;97m"
+# define BH_BLACK		"\e[1;90m"
+# define BH_RED			"\e[1;91m"
+# define BH_GREEN		"\e[1;92m"
+# define BH_YELLOW		"\e[1;93m"
+# define BH_BLUE		"\e[1;94m"
+# define BH_MAGENTA		"\e[1;95m"
+# define BH_CYAN		"\e[1;96m"
+# define BH_WHITE		"\e[1;97m"
 
-                                    /*** BACKGROUND ***/
+									/*** BACKGROUND ***/
 
 /* normal */
-# define RED_BACK		"\e[41m"
-# define GREEN_BACK		"\e[42m"
-# define BLUE_BACK		"\e[44m"
-# define CYAN_BACK		"\e[46m"
-# define MAGENTA_BACK	"\e[45m"
-# define YELLOW_BACK	"\e[43m"
-# define BLACK_BACK		"\e[40m"
-# define WHITE_BACK		"\e[47m"
+# define RED_BG			"\e[41m"
+# define GREEN_BG		"\e[42m"
+# define BLUE_BG		"\e[44m"
+# define CYAN_BG		"\e[46m"
+# define MAGENTA_BG		"\e[45m"
+# define YELLOW_BG		"\e[43m"
+# define BLACK_BG		"\e[40m"
+# define WHITE_BG		"\e[47m"
 
 /* high intensity*/
-# define H_RED_BACK		"\e[0;101m"
-# define H_GREEN_BACK	"\e[0;102m"
-# define H_BLUE_BACK	"\e[0;104m"
-# define H_CYAN_BACK	"\e[0;106m"
-# define H_MAGENTA_BACK	"\e[0;105m"
-# define H_YELLOW_BACK	"\e[0;103m"
-# define H_BLACK_BACK	"\e[0;100m"
-# define H_WHITE_BACK	"\e[0;107m"
+# define H_RED_BG		"\e[0;101m"
+# define H_GREEN_BG		"\e[0;102m"
+# define H_BLUE_BG		"\e[0;104m"
+# define H_CYAN_BG		"\e[0;106m"
+# define H_MAGENTA_BG	"\e[0;105m"
+# define H_YELLOW_BG	"\e[0;103m"
+# define H_BLACK_BG		"\e[0;100m"
+# define H_WHITE_BG		"\e[0;107m"
 
 /* reset */
-# define RESET			"\e[0m"
+# define END			"\e[0m"
 
 #endif
