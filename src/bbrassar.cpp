153458cb316d49a5b80a40773da44fbe45f1aaa9
Benjamin Brassart <bbrassar@student.42.fr>
2022-11-19 04:37:44 +0100

    Add tests for wildcards

--- src/bbrassar.cpp
+++ src/bbrassar.cpp
@@ -6,19 +6,46 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 20:42:27 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/19 00:50:59 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/19 04:15:51 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 #include "Reply.hpp"
+#include "wildcard.h"
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
 #include <algorithm>
 
+// static void __testWildcard(std::string const& pattern, std::string const& str);
+
 int main(int argc, char const* argv[])
 {
+	// {
+	// 	std::cout << "Tests wildcard\n\n";
+	// 	std::cout << std::boolalpha;
+
+	// 	__testWildcard("*", "ABC");
+	// 	__testWildcard("*", "");
+	// 	__testWildcard("", "A");
+	// 	__testWildcard("DDD", "ABC");
+	// 	__testWildcard("AB?", "ABC");
+	// 	__testWildcard("A?C", "ABC");
+	// 	__testWildcard("*@bbrassar.fr", "benjamin@bbrassar.fr");
+	// 	__testWildcard("*.fr", "benjamin@bbrassar.fr");
+	// 	__testWildcard("*.fr", "benjamin@test.bbrassar.fr");
+	// 	__testWildcard("????", "ABC");
+	// 	__testWildcard("****", "ABC");
+	// 	__testWildcard("*@*.*", "benjamin@localhost");
+	// 	__testWildcard("*@*.*", "benjamin@localhost.local");
+	// 	__testWildcard("*?", "ABC");
+	// 	__testWildcard("?*", "ABC");
+	// 	__testWildcard("A?C", "ABC");
+
+	// 	return 0;
+	// }
+
 	Server server;
 	Client client = Client(server);
 	std::ifstream input;
@@ -49,3 +76,11 @@ int main(int argc, char const* argv[])
 	while (std::getline(input, line))
 		server.processCommand(client, line);
 }
+
+// static void __testWildcard(std::string const& pattern, std::string const& str)
+// {
+// 	std::cout
+// 		<< "Pattern: '" << pattern << '\'' << '\n'
+// 		<< "String:  '" << str << '\'' << '\n'
+// 		<< "Matches: " << wildcardMatch(pattern, str) << '\n' << '\n';
+// }
