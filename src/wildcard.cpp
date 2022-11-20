1f945b2ae69315ed5188b90d5aa657f98997aa4b
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-20 23:35:01 +0100

    Fix wildcardMatch not skipping successive  '?' and '*' characters

--- src/wildcard.cpp
+++ src/wildcard.cpp
@@ -6,14 +6,14 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 01:48:21 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/19 02:37:22 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/20 23:33:51 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wildcard.h"
 #include <algorithm>
 
-static bool __wildcardMatch(std::string const& pattern, std::string const& str)
+bool wildcardMatch(std::string const& pattern, std::string const& str)
 {
 	std::string::const_reverse_iterator pat;
 	std::string::const_reverse_iterator s;
@@ -22,7 +22,13 @@ static bool __wildcardMatch(std::string const& pattern, std::string const& str)
 	{
 		if (*pat == '*')
 		{
-			if (++pat == pattern.rend())
+			do
+			{
+				++pat;
+			}
+			while (pat != pattern.rend() && (*pat == '*' || *pat == '?'));
+
+			if (pat == pattern.rend())
 				return true;
 			while (s != str.rend() && *s != *pat)
 				++s;
@@ -31,16 +37,4 @@ static bool __wildcardMatch(std::string const& pattern, std::string const& str)
 			return false;
 	}
 	return (s == str.rend() && pat == pattern.rend());
-}
-
-bool wildcardMatch(std::string const& pattern, std::string const& str)
-{
-	std::string rPattern = pattern;
-	std::string rStr = str;
-
-	std::reverse(rPattern.begin(), rPattern.end());
-	std::reverse(rStr.begin(), rStr.end());
-
-	// TODO it works but we should find something for elegant
-	return __wildcardMatch(pattern, str) || __wildcardMatch(rPattern, rStr);
 }
