5bb8afb342cfcb62518e6dd391766537913a70e3
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-21 08:09:50 +0100

    Add delimiter parameter for splitting command arguments

--- incl/command.h
+++ incl/command.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:40:51 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/19 03:39:18 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/21 02:00:23 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ struct CommandContext
 	CommandContext(Client& client, std::string const& prefix, std::string const& line);
 	~CommandContext();
 
-	static std::vector< std::string > splitArguments(std::string const& line);
+	static std::vector< std::string > splitArguments(std::string const& line, char delim = ' ');
 }; // struct CommandContext
 
 typedef void (*CommandHandler)(CommandContext& context);
@@ -38,5 +38,6 @@ void cmd_user(CommandContext& context);
 void cmd_nick(CommandContext& context);
 void cmd_quit(CommandContext& context);
 void cmd_join(CommandContext& context);
+void cmd_mode(CommandContext& context);
 
 #endif // COMMAND_H
