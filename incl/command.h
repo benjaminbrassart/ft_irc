95060ac785dbe4874d64f8e7cf542b475032a4db
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-21 11:59:54 +0100

    Add command MOTD and OPER to command.h

--- incl/command.h
+++ incl/command.h
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:40:51 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/21 02:00:23 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/21 11:50:47 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,7 @@ void cmd_nick(CommandContext& context);
 void cmd_quit(CommandContext& context);
 void cmd_join(CommandContext& context);
 void cmd_mode(CommandContext& context);
+void cmd_motd(CommandContext& context);
+void cmd_oper(CommandContext& context);
 
 #endif // COMMAND_H
