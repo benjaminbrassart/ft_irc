96eb5694a12afd8d4ecc402474cd415d73fbb95d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-11-23 01:16:32 +0100

    Improve argument splitting

--- src/CommandContext.cpp
+++ src/CommandContext.cpp
@@ -6,13 +6,15 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 11:47:46 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/22 23:31:25 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/11/23 01:14:57 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 #include "CommandMap.hpp"
 #include <algorithm>
+
+#define SIZEOF_ARRAY(Array) (sizeof (Array) / sizeof (*Array))
 
 CommandContext::CommandContext(Client& client, std::string const& line) :
 	client(client),
@@ -27,20 +29,38 @@ CommandContext::~CommandContext()
 CommandContext::ArgumentList CommandContext::split(std::string const& line, char delim)
 {
 	std::string args[15];
+	std::string first;
 	std::string::const_iterator it;
+	std::string::const_iterator first_it;
 	std::string::const_iterator fast;
 	unsigned int count;
 
 	count = 0;
-	it = line.begin();
-	while (count < (sizeof (args) / sizeof (*args)) && it != line.end())
+
+	it = std::find(line.begin(), line.end(), ':');
+
+	if (it != line.begin())
 	{
-		if (*it == ':')
+		if (it != line.end())
+			--it;
+		first_it = line.begin();
+		while (count < SIZEOF_ARRAY(args) && first_it != it)
 		{
-			args[count++] = std::string(++it, line.end());
-			break;
+			fast = std::find(first_it, it, delim);
+			args[count++] = std::string(first_it, fast);
+			first_it = fast;
+			if (first_it != it)
+				++first_it;
 		}
-		fast = std::find(it, line.end(), delim);
+		if (it != line.end())
+			++it;
+	}
+
+	while (count < SIZEOF_ARRAY(args) && it != line.end())
+	{
+		if (it != line.end() && *it == ':')
+			++it;
+		fast = std::find(it, line.end(), ':');
 		args[count++] = std::string(it, fast);
 		it = fast;
 		if (it != line.end())
