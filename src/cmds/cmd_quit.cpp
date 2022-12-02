f37b41cc9a4d5e158b731d23f5e09761de518825
Benjamin BRASSART <bbrassar@student.42.fr>
2022-12-02 15:46:37 +0100

    Continue command QUIT

--- src/cmds/cmd_quit.cpp
+++ src/cmds/cmd_quit.cpp
@@ -6,20 +6,39 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 13:50:33 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/11/16 18:44:44 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/12/02 15:17:21 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
+#include "Channel.hpp"
 #include "CommandMap.hpp"
+#include <set>
 
 void cmd_quit(CommandContext& context)
 {
+	CommandContext::ArgumentList& args = context.args;
 	Client& client = context.client;
-	Client::ChannelList::iterator it;
+	Server& server = context.server;
+	std::string const prefix = client.asPrefix();
+	Client::ChannelList::iterator chanIt;
+	Channel::ClientList::iterator clientIt;
+	std::set< Client* > recipients;
+	std::set< Client* >::iterator recipIt;
+	std::string reason;
 
-	for (it = client.channels.begin(); it != client.channels.end(); ++it)
-		(*it)->removeClient(client);
-	client.send("ERROR :buh-bye"); // TODO define message
+	if (!args.empty())
+		reason = args[0];
+
+	for (chanIt = client.channels.begin(); chanIt != client.channels.end(); ++chanIt)
+	{
+		(*chanIt)->removeClient(client);
+		for (clientIt = (*chanIt)->allClients.begin(); clientIt != (*chanIt)->allClients.end(); ++clientIt)
+			recipients.insert(clientIt->client);
+	}
+	for (recipIt = recipients.begin(); recipIt != recipients.end(); ++recipIt)
+		(*recipIt)->send(":" + prefix + " QUIT :Quit: " + reason);
+	client.send("ERROR :buh-bye"); // TODO use a better message
 	client.closeConnection();
+	server.nickManager.unregisterNickname(client.nickname);
 }
