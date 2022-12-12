a46843227c774990bfae88d471d19d4352e13c01
Benjamin BRASSART <bbrassar@student.42.fr>
2022-12-12 22:46:01 +0100

    Remove client parameter to ReplyFactory::makeReply specializations

--- incl/Reply.hpp
+++ incl/Reply.hpp
@@ -6,7 +6,7 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 01:45:10 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/12/05 12:26:33 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/12/12 21:11:38 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,391 +89,391 @@ template<>
 struct ReplyFactory<RPL_WELCOME>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& nickname, std::string const& user, std::string const& host);
+	static std::string makeReply(std::string const& nickname, std::string const& user, std::string const& host);
 };
 
 template<>
 struct ReplyFactory<RPL_YOURHOST>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<RPL_CREATED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& startDate);
+	static std::string makeReply(std::string const& startDate);
 };
 
 template<>
 struct ReplyFactory<RPL_MYINFO>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& serverName);
+	static std::string makeReply(std::string const& serverName);
 };
 
 template<>
 struct ReplyFactory<RPL_LIST>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel, std::string const& visibleClients, std::string const& topic);
+	static std::string makeReply(std::string const& channel, std::string const& visibleClients, std::string const& topic);
 };
 
 template<>
 struct ReplyFactory<RPL_LISTEND>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<RPL_CHANNELMODEIS>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel, std::string const& mode, std::string const& params);
+	static std::string makeReply(std::string const& channel, std::string const& mode, std::string const& params);
 };
 
 template<>
 struct ReplyFactory<RPL_UNIQOPIS>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel, std::string const& nickname);
+	static std::string makeReply(std::string const& channel, std::string const& nickname);
 };
 
 template<>
 struct ReplyFactory<RPL_NOTOPIC>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<RPL_TOPIC>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel, std::string const& topic);
+	static std::string makeReply(std::string const& channel, std::string const& topic);
 };
 
 template<>
 struct ReplyFactory<RPL_NAMREPLY>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& symbol, std::string const& channel, std::string const& clientList);
+	static std::string makeReply(std::string const& symbol, std::string const& channel, std::string const& clientList);
 };
 
 template<>
 struct ReplyFactory<RPL_ENDOFNAMES>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<RPL_MOTD>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& text);
+	static std::string makeReply(std::string const& text);
 };
 
 template<>
 struct ReplyFactory<RPL_MOTDSTART>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& serverName);
+	static std::string makeReply(std::string const& serverName);
 };
 
 template<>
 struct ReplyFactory<RPL_ENDOFMOTD>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 template<>
 struct ReplyFactory<RPL_YOUREOPER>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_NOSUCHNICK>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& nickname);
+	static std::string makeReply(std::string const& nickname);
 };
 
 template<>
 struct ReplyFactory<ERR_NOSUCHCHANNEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_CANNOTSENDTOCHAN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_TOOMANYCHANNELS>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_TOOMANYTARGETS>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& target, std::string const& errorCode, std::string const& abortMessage);
+	static std::string makeReply(std::string const& target, std::string const& errorCode, std::string const& abortMessage);
 };
 
 template<>
 struct ReplyFactory<ERR_NOORIGIN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_NOTEXTTOSEND>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_NOTOPLEVEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_WILDTOPLEVEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& mask);
+	static std::string makeReply(std::string const& mask);
 };
 
 template<>
 struct ReplyFactory<ERR_BADMASK>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& mask);
+	static std::string makeReply(std::string const& mask);
 };
 
 template<>
 struct ReplyFactory<ERR_UNKNOWNCOMMAND>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& command);
+	static std::string makeReply(std::string const& command);
 };
 
 template<>
 struct ReplyFactory<ERR_NOMOTD>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_FILEERROR>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& operation, std::string const& name);
+	static std::string makeReply(std::string const& operation, std::string const& name);
 };
 
 template<>
 struct ReplyFactory<ERR_NONICKNAMEGIVEN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_ERRONEUSNICKNAME>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& nickname);
+	static std::string makeReply(std::string const& nickname);
 };
 
 template<>
 struct ReplyFactory<ERR_NICKNAMEINUSE>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& nickname);
+	static std::string makeReply(std::string const& nickname);
 };
 
 template<>
 struct ReplyFactory<ERR_UNAVAILRESOURCE>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& resource);
+	static std::string makeReply(std::string const& resource);
 };
 
 template<>
 struct ReplyFactory<ERR_USERNOTINCHANNEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& nickname, std::string const& channel);
+	static std::string makeReply(std::string const& nickname, std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_NOTONCHANNEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_USERONCHANNEL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& user, std::string const& channel);
+	static std::string makeReply(std::string const& user, std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_NOLOGIN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& user);
+	static std::string makeReply(std::string const& user);
 };
 
 template<>
 struct ReplyFactory<ERR_SUMMONDISABLED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_USERSDISABLED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_NEEDMOREPARAMS>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& command);
+	static std::string makeReply(std::string const& command);
 };
 
 template<>
 struct ReplyFactory<ERR_ALREADYREGISTRED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_NOPERMFORHOST>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_PASSWDMISMATCH>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_YOUREBANNEDCREEP>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_YOUWILLBEBANNED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_KEYSET>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_CHANNELISFULL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_UNKNOWNMODE>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& c, std::string const& channel);
+	static std::string makeReply(std::string const& c, std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_INVITEONLYCHAN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_BANNEDFROMCHAN>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_BADCHANNELKEY>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_BADCHANMASK>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_NOCHANMODES>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 template<>
 struct ReplyFactory<ERR_BANLISTFULL>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel, std::string const& c);
+	static std::string makeReply(std::string const& channel, std::string const& c);
 };
 
 template<>
 struct ReplyFactory<ERR_NOPRIVILEGES>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client);
+	static std::string makeReply();
 };
 
 template<>
 struct ReplyFactory<ERR_CHANOPRIVSNEEDED>
 {
 	static std::string const NAME;
-	static std::string makeReply(Client& client, std::string const& channel);
+	static std::string makeReply(std::string const& channel);
 };
 
 #endif // REPLY_H
