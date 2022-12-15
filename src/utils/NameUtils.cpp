1c766e4def7e572d8ff15f03386f84d248bb1669
Benjamin BRASSART <bbrassar@student.42.fr>
2022-12-15 02:51:06 +0100

    Add NameUtils

---
+++ src/utils/NameUtils.cpp
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   NameUtils.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/15 01:55:43 by bbrassar          #+#    #+#             */
+/*   Updated: 2022/12/15 02:29:29 by bbrassar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils/NameUtils.hpp"
+
+static void __showClient(Client& sender, Channel::ClientList::iterator const& it, Channel& channel, bool isMember);
+
+void NameUtils::showAllNames(Client& client)
+{
+	Server& server = *client.server;
+	ChannelManager::iterator it = server.channelManager.begin();
+	RequestList reqs;
+	Request req;
+
+	for (; it != server.channelManager.end(); ++it)
+	{
+		req.name = it->name;
+		req.channel = &*it;
+		reqs.push_back(req);
+	}
+
+	showNames(client, reqs);
+}
+
+void NameUtils::showNames(Client& client, RequestList& requests)
+{
+	RequestList::iterator it = requests.begin();
+
+	for (; it != requests.end(); ++it)
+		showName(client, *it);
+}
+
+void NameUtils::showName(Client& client, Request& request)
+{
+	std::string const* namePtr = &request.name;
+	Channel::ClientList::iterator clientIt;
+
+	if (request.channel != NULL)
+	{
+		Channel& channel = *request.channel;
+		bool isMember = channel.hasClient(client);
+
+		if (isMember || (channel.mode & MODE_SECRET) == 0)
+		{
+			namePtr = &channel.name;
+			clientIt = channel.allClients.begin();
+			for (; clientIt != channel.allClients.end(); ++clientIt)
+				__showClient(client, clientIt, channel, isMember);
+		}
+	}
+
+	client.reply<RPL_ENDOFNAMES>(*namePtr);
+}
+
+static void __showClient(Client& sender, Channel::ClientList::iterator const& it, Channel& channel, bool isMember)
+{
+	Client& client = *it->client;
+	std::stringstream ss;
+
+	if (isMember || !client.isInvisible)
+	{
+		switch (it->privilege)
+		{
+			case PRIV_UNIQOP:
+			case PRIV_CHANOP: ss << '@'; break;
+			case PRIV_VOICE:  ss << '+'; break;
+			default: break;
+		}
+
+		std::string symbol;
+
+		if (channel.mode & MODE_SECRET)
+			symbol = "@";
+		else
+			symbol = "=";
+
+		ss << it->client->nickname;
+		sender.reply<RPL_NAMREPLY>(symbol, channel.name, ss.str());
+	}
+}
